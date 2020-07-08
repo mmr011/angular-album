d704e9447b4fa56820689de8fb7d6261fff665c3
Martín Molina <mmolina@nd-soft.com>
2020-07-08 18:29:03 -0400

    format product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,7 @@
-import {
-  Injectable
-} from '@angular/core';
-import {
-  Http,
-  Response
-} from '@angular/http'
-import {
-  map
-} from 'rxjs/operators/map';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http'
+import { map } from 'rxjs/operators/map';
+
 @Injectable()
 export class ProductService {
 
